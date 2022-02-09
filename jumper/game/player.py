8b7e325bb701d33af0cf600d418b232c4040ff2e
Johanna Schick <77026865+johannelwoo@users.noreply.github.com>
2022-02-08 19:57:08 -0700

    Added comments to Player.py

--- jumper/game/player.py
+++ jumper/game/player.py
@@ -1,10 +1,13 @@
+""" This class creates the user input to have the user enter their guess for the secret word."""
 
 class Player:
 
     def __init__(self):
+        """Create the initial guess as an empty string."""
         self.letter_guess = ""
 
     def guess(self):
+        """Get the user input for their guess of what a letter is in the secret word."""
         self.letter_guess = input("Guess a letter [a-z]: ")
         return (self.letter_guess)
 
